b1c0bc0cb328b4c4057c16de7664745c674de9de
Nikola <nikola.zagorchev@gmail.com>
2013-02-22 00:01:34 +0200

    Added Call and CallHistory property

---
+++ C#/OOP Classes 1/Call & CallHistory/Call.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class Call
+    {
+        private DateTime time;
+        private string dialedNumber;
+        private uint duration;
+
+        public Call(DateTime time, string dialedNumber, uint duration)
+        {
+            this.time = time;
+            this.dialedNumber = dialedNumber;
+            this.duration = duration;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}\n{1} - {2}",this.dialedNumber,this.time,this.duration);
+        }
+
+        public DateTime Time
+        {
+            get { return this.time; }
+            set { this.time = value; }
+        }
+        public string DialedNumber 
+        {
+            get { return this.dialedNumber; }
+            set
+            {
+                if (value.Length >= 5)
+                {
+                    this.dialedNumber = value;
+                }
+            }
+        }
+
+        public uint Duration
+        {
+            get { return this.duration; }
+            set
+            {
+                if (value>0 && value<uint.MaxValue)
+                {
+                    this.duration = value;
+                }
+            }
+        }
+        
+    }
+
---
+++ C#/OOP Classes 1/Call & CallHistory/GSMTest.cs
@@ -0,0 +1,16 @@
+﻿//Write a class GSMTest to test the GSM class
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class GSMTest
+    {
+        static void Main()
+        {
+            
+        }
+
+    }
+
