15ae45a7fb99b8c6c966580775ba51fefd2d768c
Nikola <nikola.zagorchev@gmail.com>
2013-02-21 23:41:23 +0200

    Added Iphone and GSMTest

---
+++ C#/OOP Classes 1/Iphone & GSMTest/Battery.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public enum BatteryType 
+    { 
+        Li_Ion, NiMH, NiCd
+    }
+    public class Battery
+    {
+        private string model;
+        private BatteryType type;
+        private byte? hoursIdle;
+        private byte? hoursTalk;
+
+        public BatteryType Type
+        {
+            get { return this.type; }
+            set { this.type = value; }
+        }
+
+        public Battery()
+        {
+        }
+        public Battery(string model, byte? hoursIdle)         
+        {
+            this.model = model;
+            this.hoursIdle = hoursIdle;
+
+        }
+        public Battery(string model, byte? hoursIdle, byte? hoursTalk,BatteryType type): this(model, hoursIdle)
+        {
+            this.hoursTalk = hoursTalk;
+            this.type = type;
+        }
+
+
+        public string Model
+        {
+            get { return this.model; }
+            set{
+                if (value.Length > 0 || value == null)
+                {
+                    this.model = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Wrong input");
+                }
+            }
+        }
+        public byte? HoursIdle
+        {
+            get { return this.hoursIdle; }
+            set
+            {
+                if ((value > 0 && value < byte.MaxValue) || value == null)
+                {
+                    this.hoursIdle = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Wrong input");
+                }
+            }
+        }
+        public byte? HoursTalk
+        {
+            get { return this.hoursTalk; }
+            set
+            {
+                if ((value > 0 && value < byte.MaxValue) || value == null)
+                {
+                    this.hoursTalk = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Wrong input");
+                }
+            }
+        }
+        
+    }
+
---
+++ C#/OOP Classes 1/Iphone & GSMTest/GSMTest.cs
@@ -0,0 +1,29 @@
+﻿//Write a class GSMTest to test the GSM class
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class GSMTest
+    {
+        static void Main()
+        {
+            GSM[] tests = new GSM[3];
+            GSM cellPhone1 = new GSM("Desire X", "HTC", "HTC Store", 499.99M);
+            tests[0] = cellPhone1;
+            GSM cellPhone2 = new GSM("Lumia 810","Nokia");
+            cellPhone2.Price = 499.99M;
+            cellPhone2.Owner = "OVI Store";
+            cellPhone2.battery.Type = BatteryType.Li_Ion;
+            tests[1] = cellPhone2;
+            GSM cellPhone3 = GSM.iPhone;
+            tests[2] = cellPhone3;
+            foreach (var item in tests)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+
