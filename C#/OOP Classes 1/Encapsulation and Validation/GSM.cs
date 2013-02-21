a2b78d5cfb8b5cfb6ae875ea35eccc709861f1ac
Nikola <nikola.zagorchev@gmail.com>
2013-02-21 23:14:54 +0200

    Added Validation

---
+++ C#/OOP Classes 1/Encapsulation and Validation/GSM.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class GSM
+{
+    public Battery battery = new Battery();
+    public Display display = new Display();
+
+    private string model;
+    private string manufacturer;
+    private string owner;
+    private decimal? price;
+
+    public GSM(string model, string manufacturer)
+        : this(model, manufacturer, null, null)
+    {
+    }
+    public GSM(string model, string manufacturer, string owner)
+        : this(model, manufacturer, owner, null)
+    {
+    }
+    public GSM(string model, string manufacturer, string owner, decimal? price)
+    {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.price = price;
+        this.owner = owner;
+    }
+    public GSM(string model, string manufacturer, string owner, decimal? price, Battery battery, Display display)
+        : this(model, manufacturer, owner, price)
+    {
+        this.battery = battery;
+        this.display = display;
+    }
+
+
+    public string Model
+    {
+        get { return this.model; }
+        set
+        {
+            if (value.Length > 0 || value == null)
+            {
+                this.model = value;
+            }
+            else
+            {
+                throw new ArgumentException("Wrong input");
+            }
+        }
+    }
+    public string Manufacturer
+    {
+        get { return this.manufacturer; }
+        set
+        {
+            if (value.Length > 0 || value == null)
+            {
+                this.manufacturer = value;
+            }
+            else
+            {
+                throw new ArgumentException("Wrong input");
+            }
+        }
+    }
+    public string Owner
+    {
+        get { return this.owner; }
+        set
+        {
+            if (value.Length > 0 || value == null)
+            {
+                this.owner = value;
+            }
+            else
+            {
+                throw new ArgumentException("Wrong input");
+            }
+        }
+    }
+    public decimal? Price
+    {
+        get { return this.price; }
+        set
+        {
+            if ((value > 0 && value < decimal.MaxValue) || value == null)
+            {
+                this.price = value;
+            }
+            else
+            {
+                throw new ArgumentException("Wrong input");
+            }
+        }
+    }
+
+    public override string ToString()
+    {
+        return string.Format("Model: {0}, Manufucturer: {1}, Owner: {2}, \nPrice: {3}",
+this.model, this.manufacturer, this.owner, this.price, this.price);
+    }
+
+}
+
---
+++ C#/OOP Classes 1/Encapsulation and Validation/Test.cs
@@ -0,0 +1,29 @@
+﻿//Use properties to encapsulate the data fields inside the GSM, Battery and Display classes. 
+//Ensure all fields hold correct data at any given time.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class Test
+    {
+        static void Main()
+        {
+            GSM cellPhone = new GSM("ssss", "aaaa","jj");
+            cellPhone.Manufacturer = "Nokia";
+            cellPhone.Model = "N86 8MP";
+            cellPhone.Owner = "Myself and I";
+            cellPhone.Price = 1234;
+            cellPhone.battery.Model = "NBN86-08";
+            cellPhone.battery.HoursIdle = 96;
+            cellPhone.battery.HoursTalk = 24;
+            cellPhone.display.Size = 2.5f;
+            cellPhone.battery.Type = BatteryType.NiCd;
+            cellPhone.display.NumberOfColors = 16.5f;
+            Display testDisplay = new Display(14, 16.5f);
+            GSM myPhone = new GSM("test", "test", "Owner", 1234);
+            Console.WriteLine(cellPhone);
+        }
+    }
+
